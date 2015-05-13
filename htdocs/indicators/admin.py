bbb029b28ed8c36e9af5dd7b3b0b04e7c3841417
Greg Lind <gwlind@gmail.com>
2015-05-13 10:15:01 +0430

    fix redirect for delete

--- htdocs/indicators/admin.py
+++ htdocs/indicators/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from indicators.models import IndicatorType, Indicator
+from indicators.models import IndicatorType, Indicator, ReportingFrequency, DisaggregationType
 
 admin.site.register(IndicatorType)
 admin.site.register(Indicator)
+admin.site.register(ReportingFrequency)
+admin.site.register(DisaggregationType)
