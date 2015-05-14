ae51a2d4d1ca5916815d714113d757480ce50a78
Greg Lind <gwlind@gmail.com>
2015-05-14 10:59:34 +0430

    update filter for sector and add sector to Programs

--- htdocs/activitydb/migrations/0002_auto_20150513_2329.py
+++ htdocs/activitydb/migrations/0002_auto_20150513_2329.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('activitydb', '0007_auto_20150513_2202'),
+        ('activitydb', '0001_initial'),
     ]
 
     operations = [
@@ -21,12 +21,6 @@ class Migration(migrations.Migration):
             model_name='community',
             name='avg_household_size',
             field=models.DecimalField(null=True, verbose_name=b'Average Household Size', max_digits=25, decimal_places=14, blank=True),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='profiletype',
-            name='profile',
-            field=models.CharField(max_length=255, verbose_name=b'Profile Name', blank=True),
             preserve_default=True,
         ),
     ]
